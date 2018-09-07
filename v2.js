bb6fb55b58f04c62f3ff460c8cd53e0e860e6213
Brycen Dorsay <bdorsay@baileynelson.com>
2018-09-07 16:30:12 -0700

    remove unnecessary dependencies

--- v2.js
+++ v2.js
@@ -1,69 +1,41 @@
-const serverMAC = 'b8:27:eb:d6:e5:5e';
+let longPresses = 0;
+let shortPresses = 0;
 
-let txCharacteristic = null;
-let connectedDevice = null;
-let timeout = null;
-let busy = false;
+function handle(e) {
 
-function connect() {
-  return NRF.connect(`${serverMAC} public`).then(device => {
-    connectedDevice = device;
-    return device.getPrimaryService('b8e0494f-3386-4512-bf2c-ea85c4cf32c0');
-  }).then(service => {
-    return service.getCharacteristic('36eef55d-900a-4e8f-b97d-67a7732aaa07');
-  }).then(characteristic => {
-    txCharacteristic = characteristic;
-    return Promise.resolve();
-  }).catch(function(err) {
-    console.log(err);
-    digitalPulse(LED1, 1, 500);
-    disconnect();
-  });
-}
-
-function disconnect() {
-  connectedDevice.disconnect();
-  connectedDevice = null;
-  digitalPulse(LED3, 1, 100);
-}
-
-function write(data) {
-  txCharacteristic.writeValue(data).then(function() {
-    digitalPulse(LED2, 1, 1000);
-  }).catch(function(err) {
-    console.log(err);
-    digitalPulse(LED1, 1, 1000);
-  });
-}
-
-function manageTimeout() {
-  if(timeout !== null) clearTimeout(timeout);
-
-  timeout = setTimeout(() => {
-    disconnect();
-    return;
-  }, 10000);
-}
-
-
-setWatch(e => {
   const pressDuration = e.time - e.lastTime;
-  const command = pressDuration <= 0.7 ? 'short' : 'long';
-
-  if(busy) return;
-  if(!connectedDevice) {
-    busy = true;
-    digitalPulse(LED3, 1, [1000, 500, 1000 ]);
-    connect().then(() => {
-      write(command);
-      manageTimeout();
-    });
-    busy = false;
-    
-    
+  if(pressDuration <= 0.5) {
+    ++shortPresses;
+    digitalPulse(LED2, 1, 500);
+    if(shortPresses > 1000) shortPresses = 1;
   } else {
-    write(command);
-    manageTimeout();
+    ++longPresses;
+    digitalPulse(LED2, 1, 1000);
+    if(longPresses > 1000) shortPresses = 1;
   }
 
-}, BTN, { edge: 'falling', debounce: 50, repeat: true });
+  NRF.setAdvertising([{
+    0x180F: Puck.getBatteryPercentage(),
+    0x1809: Math.round(E.getTemperature()),
+  }], {
+    interval: 400,
+    manufacturer: 0x0590,
+    manufacturerData: [ shortPresses, longPresses ],
+  });
+}
+
+NRF.setAdvertising([{
+  0x180F: Puck.getBatteryPercentage(),
+  0x1809: Math.round(E.getTemperature()),
+}], {
+  interval: 400,
+  manufacturer: 0x0590,
+  manufacturerData: [ shortPresses, longPresses ],
+});
+
+setWatch(handle, BTN, {
+  edge: 'falling',
+  repeat: 1,
+  debounce: 50,
+});
+
